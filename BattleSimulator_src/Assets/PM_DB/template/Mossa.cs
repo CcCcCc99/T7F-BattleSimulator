c1fbc1ba28b3b29a204b272239f1c6b5c9e6f0f5
Carlo Corvaglia <carlox03.cc@gmail.com>
2021-03-25 13:53:17 +0100

    Inserimento di semplici mosse

--- BattleSimulator_src/Assets/PM_DB/template/Mossa.cs
+++ BattleSimulator_src/Assets/PM_DB/template/Mossa.cs
@@ -1,19 +1,9 @@
 using UnityEngine;
 
-enum Categoria {
-    Fisico,
-    Speciale,
-    Stato
-}
-
-[CreateAssetMenu(fileName = "Nuova mossa", menuName = "Pokémon/Mossa")]
 public class Mossa : ScriptableObject
 {
     [SerializeField] string nome;
     [SerializeField] Tipo tipo;
-    [SerializeField] Categoria categoria;
-
-    [SerializeField] int pp;
-    [SerializeField] int dannoBase;
+    [SerializeField] int puntiPotere;
     [SerializeField] int precisione;
 }
---
+++ BattleSimulator_src/Assets/PM_DB/template/MossaAttacco.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Categoria
+{
+    Fisico,
+    Speciale
+}
+
+[CreateAssetMenu(fileName = "Nuova mossa attacco", menuName = "Pokémon/Mossa Attacco")]
+public class MossaAttacco : Mossa
+{
+    [SerializeField] Categoria categoria;
+    [SerializeField] int dannoBase;
+}
