a03c14580a369ff1b0f5a35c588526e69ad517c7
Carlo Corvaglia <carlox03.cc@gmail.com>
2021-03-27 00:40:32 +0100

    Agginti i getter alla classe tipo (non definitivo)

--- BattleSimulator_src/Assets/PM_DB/template/Tipo.cs
+++ BattleSimulator_src/Assets/PM_DB/template/Tipo.cs
@@ -12,11 +12,33 @@ public class Tipo : ScriptableObject
     [SerializeField] List<Tipo> resistenze;
     [SerializeField] List<Tipo> debolezze;
 
+    //---------------------------------------------------
+
     public float relazioneTipo(Tipo att)
     {
         if (immunità.Contains(att)) return 0f;
         else if (resistenze.Contains(att)) return 0.5f;
         else if (debolezze.Contains(att)) return 2f;
         else return 1f;
+    }
+
+    //---------------------------------------------------
+
+    public string NOME {
+        get { return nome; } 
+    }
+    public Color COLORE {
+        get { return colore; }
+    }
+    // TODO da testare assolutante cosa ritorna questa cosa
+    // magari su un progetto separato
+    public List<Tipo> IMMUNITA {
+        get { return immunità; }
+    }
+    public List<Tipo> RESISTENZA{
+        get { return resistenze; }
+    }
+    public List<Tipo> DEBOLEZZE{
+        get { return debolezze; }
     }
 }
