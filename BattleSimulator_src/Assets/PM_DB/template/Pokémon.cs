13f4c0572ec6f8f8aea1615d5f23360d009a9693
Carlo Corvaglia <carlox03.cc@gmail.com>
2021-03-27 01:09:27 +0100

    Aggiunti getter alla classe pokémon (non definitivo)

--- BattleSimulator_src/Assets/PM_DB/template/Pokémon.cs
+++ BattleSimulator_src/Assets/PM_DB/template/Pokémon.cs
@@ -19,4 +19,46 @@ public class Pokémon : ScriptableObject
     [SerializeField] int velocitàBase;
 
     [SerializeField] List<Mossa> mosseImparabili;
+
+    //---------------------------------------------------
+
+    //informazioni generali
+    public string NOME {
+        get { return nome; }
+    }
+    public int N_DEX {
+        get { return numPokédex; }
+    }
+    public Sprite F_SPRITE{
+        get { return FrontSprite; }
+    }
+    public Sprite B_SPRITE{
+        get { return BackSprite; }
+    }
+    public List<Tipo> TIPI {
+        get { return tipi; }
+    }
+    public List<Mossa> MOVESET {
+        get { return mosseImparabili; }
+    }
+
+    //statistiche
+    public int HP {
+        get { return HPBase; }
+    }
+    public int ATT {
+        get { return attFisicoBase; }
+    }
+    public int DEF{
+        get { return difFisicaBase; }
+    }
+    public int SP_ATT {
+        get { return attSpecialeBase; }
+    }
+    public int SP_DEF{
+        get { return difSpecialeBase; }
+    }
+    public int SPE {
+        get { return velocitàBase; }
+    }
 }
