315cb466719042f73bf70a13f56644f0dadbd56f
Carlo Corvaglia <carlox03.cc@gmail.com>
2021-04-03 01:25:35 +0200

    commit per sbaglio

---
+++ BattleSimulator_src/Assets/PM_DB/template/Nature.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum stat
+{
+    hp,
+    attacco,
+    difesa,
+    spAttacco,
+    spDifesa,
+    velocità
+}
+
+[CreateAssetMenu(fileName = "Nuova mossa attacco", menuName = "Pokémon/Natura")]
+public class Nature : ScriptableObject
+{
+    [SerializeField] string nome;
+    [SerializeField] stat statAumentata;
+    [SerializeField] stat statDiminuita;
+
+    public float varazioneNatura(stat statistica) 
+    {
+        if (statistica == statAumentata)
+            return 1.1f;
+        if (statistica == statDiminuita)
+            return 0.9f;
+        return 1f;
+    }
+}
